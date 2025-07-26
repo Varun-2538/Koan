e54f0ec5d88588998f93684a6b4c6f2282f754a0
Varun-2538 <942525@gmail.com>
2025-07-26 16:15:22 +0530

    landing page and template page changed

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,30 +1,7 @@
 "use client"
 
-import { useState } from "react"
-import { FlowCanvas } from "@/components/flow-canvas"
-import { ProjectSelector } from "@/components/project-selector"
-import { DemoNotice } from "@/components/demo-notice"
+import LandingHero from "@/components/landing-hero"
 
 export default function Home() {
-  const [selectedProject, setSelectedProject] = useState<string | null>(null)
-
-  if (!selectedProject) {
-    return (
-      <div className="h-screen flex flex-col">
-        <DemoNotice />
-        <div className="flex-1">
-          <ProjectSelector onProjectSelect={setSelectedProject} />
-        </div>
-      </div>
-    )
-  }
-
-  return (
-    <div className="h-screen flex flex-col">
-      <DemoNotice />
-      <div className="flex-1">
-        <FlowCanvas projectId={selectedProject} />
-      </div>
-    </div>
-  )
+  return <LandingHero />
 }
